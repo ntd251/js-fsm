1bd366bfff15770001e83615184f488b034a7944
Duong Nguyen <ntd251@users.noreply.github.com>
2017-09-23 22:13:55 +0800

    Added requirements and examples

---
+++ example.js
@@ -0,0 +1,83 @@
+let addFuel = new AddFuel(100);
+
+let payload = {
+  startWith: {
+    state: 'pending',
+    stateData: 'HAS_FUEL',
+  },
+  when: {
+    pending: [
+      {
+        event: 'START',
+        stateData: 'HAS_FUEL',
+        goto: 'started',
+        using: 'startEngine',
+        stateTimeout: 5000,
+      }
+    ],
+
+    // Alternative
+    pending: [
+      {
+        event: 'START',
+        stateDataSwitch: {
+          HAS_FUEL: {
+            goto: 'started',
+            using: 'startEngine',
+          },
+          else: {
+            stay: true,
+          }
+        },
+      },
+      {
+        event: AddFuel,
+        stateData: 'any',
+        stay: true,
+        using: 'addFuel',
+      }
+    ],
+
+    else: [
+
+    ],
+  },
+
+  afterTransition: [
+    {
+      from: 'pending',
+      to: 'started',
+      do: 'speedUp',
+    },
+  ],
+
+  // Alternative
+  afterTransition: {
+    'a -> b': 'speedUp', // not so good
+  }
+
+  actionStore: {
+    startEngine: (event) => {
+      let stateData = 'HasFuel';
+      return stateData;
+    },
+  }
+};
+
+let fsm = new FSM(config);
+
+fsm.updateActionStore({
+  startEngine: (event) => {
+
+  },
+});
+
+fsm.initialize();
+
+fsm.state; // pending
+fsm.stateData; // HAS_FUEL
+
+fsm.receive('start');
+fsm.state; // started
+fsm.stateData; // HAS_FUEL
+
