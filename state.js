cf3919e0c188266238821fffd17108c4a5c59153
Duong Nguyen <ntd251@users.noreply.github.com>
2017-09-24 00:46:07 +0800

    Added State and StateData

---
+++ state.js
@@ -0,0 +1,7 @@
+class State {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+export default State;
---
+++ state_data.js
@@ -0,0 +1,33 @@
+class StateData {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+const ANY = new StateData('Any');
+
+StateData.Any = ANY;
+
+StateData.AnyMatches = [
+  undefined,
+  '_',
+  'any',
+  'else',
+];
+
+/**
+ * Convert AnyMatches to StateData.Any
+ * @param  {Any} stateData
+ * @return {StateData}
+ */
+StateData.normalize = function(stateData) {
+  if (StateData.AnyMatches.includes(stateData)) {
+    return StateData.Any;
+  }
+  
+  if (stateData.constructor.toString() === 'StateData') {
+    return stateData;
+  }
+};
+
+export default StateData;
