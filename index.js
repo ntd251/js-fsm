9a6c935fe7f3864c172ea62f8a16e726db6bd47c
Duong Nguyen <ntd251@users.noreply.github.com>
2017-09-24 22:50:39 +0800

    Made classes available through FSM namespace

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-import FSM from './lib/fsm';
-window.FSM = FSM;
+import State from './lib/state';
+import StateData from './lib/state_data';
+import StateEvent from './lib/state_event';
+import StateMachine from './lib/state_machine';
+import FsmCore from './lib/fsm_core';
+
+window.FSM = {
+  StateMachine: StateMachine,
+  State: State,
+  StateData: StateData,
+  StateEvent: StateEvent,
+  Core: FsmCore,
+};
